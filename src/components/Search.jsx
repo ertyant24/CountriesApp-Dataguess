60a41270ca524f5d2fbb433c4c3246a9c47611ef
ertyant24 <erikciertugrul@gmail.com>
2023-09-14 01:41:55 +0300

    seçili olan div' e tekrar tıklandığında white olsun

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -19,18 +19,18 @@ function Search() {
     useEffect(() => {
         if (filterData) {
             let selectedIndex;
-      
+
             if (filterData.length <= 10) {
-              selectedIndex = filterData.length - 1;
+                selectedIndex = filterData.length - 1;
             } else {
-              selectedIndex = 9;
+                selectedIndex = 9;
             }
-      
+
             const selectedColor = colors[Math.floor(Math.random() * colors.length)];
-      
+
             setSelectedDiv({ index: selectedIndex, color: selectedColor });
-          }
-    },[filterData])
+        }
+    }, [filterData])
 
 
     const colors = ["red", "yellow", "pink", "purple", "grey", "orange", "brown", "gold", "green"];
@@ -45,76 +45,85 @@ function Search() {
     const changeBackgroundColor = (index) => {
         let randomColor;
 
-        do{
-            randomColor = colors[Math.floor(Math.random() * colors.length)]
-        } while(randomColor === previousColor)
+        // Seçili olan div'e tekrar tıkladığında beyaz olsun arka planı.
+        if (selectedDiv?.index === index) {
+            setSelectedDiv(null);
+        }
+        else{
+            do {
+                randomColor = colors[Math.floor(Math.random() * colors.length)]
+            } while (randomColor === previousColor)
 
-        setPreviousColor(randomColor);
-       
-        setSelectedDiv({ index, color: randomColor});
-    }
+            setPreviousColor(randomColor);
 
-    const deleteDiv = (code) => {
-        window.confirm(`Are you sure deleted this country - ${code}`)
-        setFilterData(() => filterData.filter((country) => country.code !== code));
-    }
-
-    const searchChange = (event) => {
-        setSearchText(event.target.value);
-    };
-
-    const search = () => {
-        const lowerCaseText = searchText.toLowerCase();
-        const filteredCountries = data.countries.filter((country) => country.name.toLowerCase().includes(lowerCaseText));
-        console.log(filteredCountries);
-        setFilterData(filteredCountries);
-    }
-
+            setSelectedDiv(index);
+        
+            setSelectedDiv({ index, color: randomColor });
+        }
    
-    return (
-        <>
-            <div className="container mt-5">
-                <div className="row">
-                    <div className="col-6 offset-3">
-                        <div className="mb-3">
-                            <label htmlFor="search" className='form-label fw-semibold'>Country Name</label>
-                            <div className='d-flex'>
-                                <input
-                                    type="text"
-                                    placeholder="Ara..."
-                                    value={searchText}
-                                    onChange={searchChange}
-                                    className='me-3 form-control'
-                                    id='search'
-                                    name='search'
-                                />
-                                <button onClick={search} className='btn btn-primary'>Ara</button>
-                            </div>
+}
 
-                            <div className="mt-5 d-flex flex-wrap justify-content-center">
-                                {filterData.map((country, index) => (
-                                    <div key={country.code} >
-                                        <div className='country mt-4' style={{ backgroundColor: selectedDiv?.index === index ? selectedDiv.color : "white", cursor: "pointer"}} onClick={() => changeBackgroundColor(index)}>
-                                            <div className='d-flex justify-content-between mb-4'>
-                                                <h2 className=''>{country.code}
-                                                </h2>
-                                                <span className=''><i onClick={() => {deleteDiv(country.code); toastr.error('Deleted this data.', `${country.name} - ${country.emoji}`, {timeOut: 3000})}} className="fa-solid fa-xmark fa-2x border border-black p-1 rounded-circle"></i></span>
-                                            </div><hr />
-                                            <p><span className='fw-semibold'>Name:</span> {country.name} - <span>{country.emoji}</span></p>
-                                            <p><span className='fw-semibold'>Phone:</span> {country.phone}</p>
-                                            <p><span className='fw-semibold'>Currency:</span> {country.currency}</p>
-                                            <p><span className='fw-semibold'>Native:</span> {country.native}</p>
-                                        </div>
-                                    </div>
-                                ))}
-                            </div>
+const deleteDiv = (code) => {
+    window.confirm(`Are you sure deleted this country - ${code}`)
+    setFilterData(() => filterData.filter((country) => country.code !== code));
+}
 
+const searchChange = (event) => {
+    setSearchText(event.target.value);
+};
+
+const search = () => {
+    const lowerCaseText = searchText.toLowerCase();
+    const filteredCountries = data.countries.filter((country) => country.name.toLowerCase().includes(lowerCaseText));
+    console.log(filteredCountries);
+    setFilterData(filteredCountries);
+}
+
+
+return (
+    <>
+        <div className="container mt-5">
+            <div className="row">
+                <div className="col-6 offset-3">
+                    <div className="mb-3">
+                        <label htmlFor="search" className='form-label fw-semibold'>Country Name</label>
+                        <div className='d-flex'>
+                            <input
+                                type="text"
+                                placeholder="Ara..."
+                                value={searchText}
+                                onChange={searchChange}
+                                className='me-3 form-control'
+                                id='search'
+                                name='search'
+                            />
+                            <button onClick={search} className='btn btn-primary'>Ara</button>
                         </div>
+
+                        <div className="mt-5 d-flex flex-wrap justify-content-center">
+                            {filterData.map((country, index) => (
+                                <div key={country.code} >
+                                    <div className='country mt-4' style={{ backgroundColor: selectedDiv?.index === index ? selectedDiv.color : "white", cursor: "pointer" }} onClick={() => changeBackgroundColor(index)}>
+                                        <div className='d-flex justify-content-between mb-4'>
+                                            <h2 className=''>{country.code}
+                                            </h2>
+                                            <span className=''><i onClick={() => { deleteDiv(country.code); toastr.error('Deleted this data.', `${country.name} - ${country.emoji}`, { timeOut: 3000 }) }} className="fa-solid fa-xmark fa-2x border border-black p-1 rounded-circle"></i></span>
+                                        </div><hr />
+                                        <p><span className='fw-semibold'>Name:</span> {country.name} - <span>{country.emoji}</span></p>
+                                        <p><span className='fw-semibold'>Phone:</span> {country.phone}</p>
+                                        <p><span className='fw-semibold'>Currency:</span> {country.currency}</p>
+                                        <p><span className='fw-semibold'>Native:</span> {country.native}</p>
+                                    </div>
+                                </div>
+                            ))}
+                        </div>
+
                     </div>
                 </div>
             </div>
-        </>
-    )
+        </div>
+    </>
+)
 }
 
 export default Search
