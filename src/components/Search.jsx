b6f513f15e0b73ba65fb35ebe07b9ad7045f053b
ertyant24 <erikciertugrul@gmail.com>
2023-09-13 19:43:55 +0300

    revize

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,7 +1,9 @@
 import React, { useState } from 'react'
-import { useQuery, gql } from '@apollo/client';
+import { useQuery } from '@apollo/client';
 import QueryCountries from '../schemas/Countries';
 import '../App.css'
+import toastr from 'toastr'
+import 'toastr/build/toastr.min.css'
 
 function Search() {
 
@@ -23,7 +25,13 @@ function Search() {
 
     const changeBackgroundColor = (index) => {
         const randomColor = colors[Math.floor(Math.random() * colors.length)]
-        setSelectedDiv({index, color: randomColor})
+        setSelectedDiv({ index, color: randomColor})
+    }
+
+    const deleteDiv = (code) => {
+        window.confirm(`Are you sure deleted this country - ${code}`)
+        setFilterData(() => filterData.filter((country) => country.code !== code));
+        // console.log(prevData);
     }
 
     const searchChange = (event) => {
@@ -39,10 +47,10 @@ function Search() {
 
     return (
         <>
-            <div className="container mt-5 pe-5">
+            <div className="container mt-5">
                 <div className="row">
                     <div className="col-6 offset-3">
-                        <div className="mb-3 ps-5">
+                        <div className="mb-3">
                             <label htmlFor="search" className='form-label fw-semibold'>Country Name</label>
                             <div className='d-flex'>
                                 <input
@@ -58,11 +66,14 @@ function Search() {
                             </div>
 
                             <div className="mt-5 d-flex flex-wrap justify-content-center">
-                                {filterData.map((country, index) => (
+                                {filterData.map((country, index, x) => (
                                     <div key={country.code} >
-                                        <div className='country mt-4' style={{backgroundColor: selectedDiv?.index === index ? selectedDiv.color: "white" , cursor: "pointer"}} onClick={() => changeBackgroundColor(index)}>
-                                            <h2 className='mb-3 text-center'>{country.code}
-                                           </h2><hr />
+                                        <div className='country mt-4' style={{ backgroundColor: selectedDiv?.index === index ? selectedDiv.color : "white", cursor: "pointer"}} onClick={() => changeBackgroundColor(index)}>
+                                            <div className='d-flex justify-content-between mb-4'>
+                                                <h2 className=''>{country.code}
+                                                </h2>
+                                                <span className=''><i onClick={() => {deleteDiv(country.code); toastr.error('Deleted this data.', `${country.name} - ${country.emoji}`, {timeOut: 3000})}} className="fa-solid fa-xmark fa-2x border border-black p-1 rounded-circle"></i></span>
+                                            </div><hr />
                                             <p><span className='fw-semibold'>Name:</span> {country.name} - <span>{country.emoji}</span></p>
                                             <p><span className='fw-semibold'>Phone:</span> {country.phone}</p>
                                             <p><span className='fw-semibold'>Currency:</span> {country.currency}</p>
